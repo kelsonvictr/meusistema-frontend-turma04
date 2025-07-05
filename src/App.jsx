6fe942b41d8a223097fd27d6f6c5648ce8fe5cc0
kelsonalmeida <kelson.victr@gmail.com>
2025-07-05 11:34:39 -0300

    fix: andamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import FornecedorForm from './pages/Fornecedor/FornecedorForm'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Inicial from './pages/Inicial'
 
 const App = () => {
   return (
-    <>
-    { /* Por enquanto, sem roteamento */ }
-      <FornecedorForm />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicial />} />
+        <Route path="/cadastrar-fornecedor" element={<FornecedorForm />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
